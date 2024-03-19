59540547e09f793adde9053595f883c1f4ac6b2f
Dominic Hiland <dominic.dev.email@gmail.com>
2024-03-18 20:16:41 -0700

    Rebuild image-carousel as a reusable component. Also added personal profile image and others

---
+++ src/app/components/image-carousel/image-carousel.component.ts
@@ -0,0 +1,55 @@
+import {Component, Input, OnDestroy, OnInit} from "@angular/core";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {NgStyle} from "@angular/common";
+
+@Component({
+  standalone: true,
+  selector: 'image-carousel',
+  imports: [
+    MatTabGroup,
+    MatTab,
+    MatIcon,
+    MatIconButton,
+    NgStyle
+  ],
+  templateUrl: 'image-carousel.component.html',
+  styleUrl: 'image-carousel.component.scss',
+})
+export class ImageCarouselComponent implements OnInit, OnDestroy {
+
+  @Input() fixedHeight?: string
+  @Input({required: true}) imageNames!: string[]
+  imageSources!: string[]
+
+  carouselInterval: any = undefined
+  carouselIndex: number = 0
+
+  ngOnInit(): void {
+    this.imageSources = this.imageNames.reduce((acc: string[], r) => [...acc, `assets/${r}`], [])
+    this.startInterval()
+  }
+
+  ngOnDestroy(): void {
+    this.stopInterval()
+  }
+
+  startInterval() {
+    this.carouselInterval = setInterval(() => {
+      this.setCarouselIndex(this.carouselIndex + 1)
+    }, 5000)
+  }
+
+  stopInterval() {
+    clearInterval(this.carouselInterval)
+  }
+
+  setCarouselIndex(index: number, reset?: boolean) {
+    if (index < 0) index = this.imageSources.length - 1 // Keep the carousel circular
+    if (reset) this.stopInterval()
+    this.carouselIndex = index % this.imageSources.length
+    if (reset) this.startInterval()
+  }
+
+}
--- src/app/components/project-wrapper/project-wrapper.component.ts
+++ src/app/components/project-wrapper/project-wrapper.component.ts
@@ -1,8 +1,14 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnDestroy, OnInit} from "@angular/core";
 import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
 import LinkButton from "../../models/LinkButton";
 import {LinkButtonComponent} from "../link-button/link-button.component";
 import {ProjectLinkWrapperComponent} from "./project-link-wrapper/project-link-wrapper.component";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatRipple} from "@angular/material/core";
+import {NgStyle} from "@angular/common";
+import {ImageCarouselComponent} from "../image-carousel/image-carousel.component";
 
 @Component({
   standalone: true,
@@ -12,7 +18,14 @@ import {ProjectLinkWrapperComponent} from "./project-link-wrapper/project-link-w
     MatCardContent,
     MatCardTitle,
     LinkButtonComponent,
-    ProjectLinkWrapperComponent
+    ProjectLinkWrapperComponent,
+    MatTabGroup,
+    MatTab,
+    MatIconButton,
+    MatIcon,
+    MatRipple,
+    NgStyle,
+    ImageCarouselComponent
   ],
   templateUrl: 'project-wrapper.component.html',
   styleUrl: 'project-wrapper.component.scss',
