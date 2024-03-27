b3cf2aef173f8e0d2e3eaae95d2df6cd25922d81
Dominic Hiland <dominic.dev.email@gmail.com>
2024-03-27 10:25:41 -0500

    Updated resume link

--- src/app/home/profile-content/profile-content.component.ts
+++ src/app/home/profile-content/profile-content.component.ts
@@ -23,7 +23,7 @@ export class ProfileContentComponent {
   buttons: LinkButton[] = [
     new LinkButton('LinkedIn', '#0077b5', 'www.linkedin.com/in/dominic-hiland-19211a192', 'linkedin-custom.png'),
     new LinkButton('GitHub', COMMON_COLORS['GITHUB'], 'www.github.com/dev-dominic-1', 'github-custom.png'),
-    new LinkButton('Resume', this.primaryColor, 'www.dropbox.com/scl/fi/0im7df2z1c59wmeghfnjb/Resume-02-16-2024.pdf?rlkey=6s1etfrz11neihwrj0vorpt8w&dl=0', 'file-document-custom.png'),
+    new LinkButton('Resume', this.primaryColor, 'www.dropbox.com/scl/fi/sxu8hib19gvgv46mfahea/Resume-03-21-2024.docx?rlkey=nzcp103yvrzofjv0twkxiczla&dl=0', 'file-document-custom.png'),
   ]
 
 }
